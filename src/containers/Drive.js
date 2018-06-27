9d867a42504da91dd2861eaeaa7eb2aef52206d2
china-dm <236882283@qq.com>
2018-06-27 23:10:08 +0800

    save

--- src/containers/Drive.js
+++ src/containers/Drive.js
@@ -2,7 +2,7 @@ import React from 'react'
 import {connect} from 'react-redux';
 import {bindActionCreators} from 'redux';
 import * as headerActions from '../actions/header'
-import {Icon, Carousel} from 'antd-mobile';
+import {Button, Carousel} from 'antd-mobile';
 import '../assets/css/orderConfirm.less';
 import '../assets/css/orderSuccess.less';
 
@@ -40,31 +40,7 @@ export default class List1 extends React.Component {
             <div className="order-confirm order-success">
                 <div className="order-subscribe-info">
                     <div className="order-sub-img">
-                        <Carousel
-                            autoplay
-                            infinite
-                            /*beforeChange
-                            afterChange*/
-                        >
-                            {this.state.data.map(val => (
-                                <a
-                                    key={val}
-                                    href="http://www.alipay.com"
-                                    style={{display: 'inline-block', width: '100%', height: this.state.imgHeight}}
-                                >
-                                    <img
-                                        src={require('../assets/images/test-car.png')}
-                                        alt=""
-                                        style={{width: '100%', verticalAlign: 'top'}}
-                                        onLoad={() => {
-                                            // fire window resize event to change height
-                                            window.dispatchEvent(new Event('resize'));
-                                            this.setState({imgHeight: 'auto'});
-                                        }}
-                                    />
-                                </a>
-                            ))}
-                        </Carousel>
+                        <img src={require('../assets/images/icon-car-logo.jpg')} />
                     </div>
                     <div className="order-sub-content">
                         <h1>C200  E300L</h1>
@@ -107,7 +83,7 @@ export default class List1 extends React.Component {
                             </li>
                             <li>
                                 <div className="order-owner-label">我的预约状态：</div>
-                                <div className="order-owner-content">未预约</div>
+                                <div className="order-owner-content"><em>未预约</em> <em className="blue">未预约</em></div>
                             </li>
                             <li>
                                 <div className="order-owner-label">联系人：</div>
@@ -121,46 +97,18 @@ export default class List1 extends React.Component {
                     </div>
                 </div>
                 <div className="wanc-module">
-                    <div className="wanc-module-title">
-                        <span className="title">实付金额</span>
-                        <span className="price">¥3000.00</span>
-                    </div>
-                    <div className="wanc-module-content">
-                        <ul className="order-owner">
-                            <li>
-                                <div className="order-owner-label">订单编号：</div>
-                                <div className="order-owner-content">987654321</div>
-                            </li>
-                            <li>
-                                <div className="order-owner-label">订单状态：</div>
-                                <div className="order-owner-content">已付款</div>
-                            </li>
-                            <li>
-                                <div className="order-owner-label">支付方式：</div>
-                                <div className="order-owner-content">支付宝支付</div>
-                            </li>
-                            <li>
-                                <div className="order-owner-label">创建时间：</div>
-                                <div className="order-owner-content">2018-05-15 15:07:37</div>
-                            </li>
-                            <li>
-                                <div className="order-owner-label">手机号：</div>
-                                <div className="order-owner-content">18652086015</div>
-                            </li>
-                        </ul>
+                    <div className="order-running-other">
+                        <span className="title">其他事项说明</span>
+                        <ol>
+                            <li>1.如发生意外事件；</li>
+                            <li>2.如提供免费午餐；</li>
+                            <li>3.如预约试驾者可领取卡丁车优惠券；</li>
+                            <li>4.其他其他其他；</li>
+                        </ol>
                     </div>
                 </div>
-                <div className="wanc-module">
-                    <div className="wanc-module-title">
-                        <div className="wanc-module-text">
-                            <span className="title">已申请</span>
-                            <img src={require('../assets/images/icon-person.jpg')}/>
-                            <img src={require('../assets/images/icon-person.jpg')}/>
-                            <img src={require('../assets/images/icon-person.jpg')}/>
-                            <img src={require('../assets/images/icon-person.jpg')}/>
-                        </div>
-                        <Icon type="right"></Icon>
-                    </div>
+                <div className="order-running-button">
+                    <Button type="primary" disbaled>我要试驾</Button>
                 </div>
             </div>
         )
